3f48ff1f02ef15ac057f31c72f99de069b3054a3
itsanti <kurov.aleksandr+github@gmail.com>
2021-12-26 15:00:26 +0300

    fix after review

--- course_575/lesson36_shop/test_items.py
+++ course_575/lesson36_shop/test_items.py
@@ -1,7 +1,12 @@
 import time
+import selenium
 
 
 def test_find_add_to_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    assert browser.find_element_by_css_selector('.btn-add-to-basket')
+    add = None
+    try:
+        add = browser.find_element_by_css_selector('.btn-add-to-basket')
+    except selenium.common.exceptions.NoSuchElementException:
+        assert add, "no button"
     time.sleep(10)
